9770018e165a35d8a8d6d42a39012d2051acc8c3
Stefan Holmberg <stefan.holmberg@systementor.se>
2023-01-06 18:01:41 +0100

    a

--- app.py
+++ app.py
@@ -4,15 +4,10 @@ from flask_migrate import Migrate, upgrade
 
 from model import db, seedData, Customer
 
-# gör om kontaktsida -> kundlist-sida (/Customers) - dvs byt namn på funktion, byt URL
-# gör om Kundlist-sidan så du har en TEMPLATE (det finns en index copy.html 
-#                  du kan kolla på så ser du ett exempel på loop)
-# Kundlist-sidan ska ha en snygg TABLE
-# Varje kund har en bild den bildens url ska vara https://img.systementor.se/<id>/300/200 - id är kundens id i databasen (numeriskt!)
-# Vid klick på en BILD ska man komma till en Bild-sida
-# På Bild-sidan ska NAMN + bild visas https://img.systementor.se/<id>/800/600
-# I Kundlist-sidans tabell ska det vara en LÄNK -> Kundsida
-# På Kundsidan visas ALL INFORMATION OM KUNDEN samt en bild https://img.systementor.se/<id>/500/400
+# active page
+# Sorting
+# paging
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:hejsan123@localhost/players0101'
@@ -22,25 +17,50 @@ migrate = Migrate(app,db)
 
 @app.route("/")
 def startpage():
-	    return render_template("index.html" )
+	    return render_template("index.html", activePage="startPage" )
 
 @app.route("/customerimage/<id>")
 def customerimagepage(id):
     customer = Customer.query.filter_by(Id = id).first()
-    return render_template("customerimage.html", customer=customer )
+    return render_template("customerimage.html", customer=customer, activePage="customersPage" )
 
 
 @app.route("/customer/<id>")
 def customerpage(id):
     customer = Customer.query.filter_by(Id = id).first()
-    return render_template("customer.html", customer=customer )
+    return render_template("customer.html", customer=customer, activePage="customersPage" )
 
 
 
 @app.route("/customers")
 def customerspage():
-    #listOfCustomers = Customer.query.all()
-    return render_template("customers.html", listOfCustomers=Customer.query.all() )
+    sortColumn = request.args.get('sortColumn', 'namn')
+    sortOrder = request.args.get('sortOrder', 'asc')
+    page = int(request.args.get('page', 1))
+
+    listOfCustomers = Customer.query
+
+    if sortColumn == "namn":
+        if sortOrder == "asc":
+            listOfCustomers = listOfCustomers.order_by(Customer.Name.asc())
+        else:
+            listOfCustomers = listOfCustomers.order_by(Customer.Name.desc())
+    elif sortColumn == "city":
+        if sortOrder == "asc":
+            listOfCustomers = listOfCustomers.order_by(Customer.City.asc())
+        else:
+            listOfCustomers = listOfCustomers.order_by(Customer.City.desc())
+
+    paginationObject = listOfCustomers.paginate(page=page,per_page=30,error_out=False )
+    return render_template("customers.html", 
+                    listOfCustomers=paginationObject.items, 
+                    activePage="customersPage",
+                    page=page,
+                    sortColumn=sortColumn,
+                    sortOrder=sortOrder,
+                    has_next = paginationObject.has_next,
+                    has_prev = paginationObject.has_prev,
+                    pages=paginationObject.pages )
 
 if __name__  == "__main__":
     with app.app_context():

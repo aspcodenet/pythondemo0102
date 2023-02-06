8de5f54d5a4a294546eb3ce0f6d1fc7bad860b43
Stefan <stefan.holmberg@systementor.se>
2023-02-06 10:22:44 +0100

    a

--- formsTest.py
+++ formsTest.py
@@ -21,21 +21,21 @@ class FormsTestCases(unittest.TestCase):
         db.init_app(app)
         db.app = app
         db.create_all()
-
+        
     def tearDown(self):
         #self.ctx.pop()
         pass
 
-    def test_when_creating_new_should_validate_name_is_longer_than_three(self):
+    def test_when_creating_new_should_validate_name_ends_with_se(self):
         test_client = app.test_client()
         with test_client:
             url = '/newcustomer'
-            response = test_client.post(url, data={ "name":"Kalle", "city":"Teststad", "age":"12", "countryCode":"SE" })
+            response = test_client.post(url, data={ "name":"Kalle", "city":"Teststad", "age":"31", "countryCode":"SE" })
             s = response.data.decode("utf-8") 
             ok = 'Måste sluta på .se dummer' in s
             self.assertTrue(ok)
 
-    def test_when_creating_new_should_be_ok_when_name_is_longer_than_three(self):
+    def test_when_creating_new_should_be_ok_when_name_is_ok(self):
         test_client = app.test_client()
         with test_client:
             url = '/newcustomer'
